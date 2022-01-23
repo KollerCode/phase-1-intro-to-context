9289c227be3bfead750a6fe5702524496c549b61
Natasha Koller <kollernatasha@gmail.com>
2022-01-22 20:06:10 -0500

    commit tree 92b3f3df46895ee3f04c319608a5044f2be9b145

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+function createEmployeeRecord(sampleEmployee) {
+    let payRollArray = []
+    return payRollArray =
+    {
+      firstName: sampleEmployee[0],
+      familyName: sampleEmployee[1],
+      title: sampleEmployee[2],
+      payPerHour: sampleEmployee[3],
+      timeInEvents: [],
+      timeOutEvents: [],
+    }
+
+}
+
+function createEmployeeRecords(payRollArray) {
+ return payRollArray.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(employee, date) {
+  const obj = {
+      type: "TimeIn",
+      hour: parseInt(date.split(' ')[1]),
+      date: date.split(' ')[0]
+    }
+  
+  employee.timeInEvents.push(obj)
+  return employee
+}
+
+function createTimeOutEvent(employee, date) {
+  const obj = {
+    type: "TimeOut",
+    hour: parseInt(date.split(" ")[1]),
+    date: date.split(" ")[0],
+  };
+
+  employee.timeOutEvents.push(obj);
+  return employee;
+}
+
+function hoursWorkedOnDate() {}
+
+function wagesEarnedOnDate() {}
+
+function allWagesFor() {}
+
+function calculatePayroll() {}
